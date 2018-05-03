90d6e62ff1df0230a8b6749038e6fcd95a0c517c
Luis Eduardo Patino Ramon <lpatino@lpatinomac.local>
2018-05-03 08:57:13 -0500

    challenge #1 bootcamp typescript

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,15 @@
 import { Component } from '@angular/core';
 
-interface Person {
-  name: string;
-  age: number;
-  twitter: string;
-}
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
-  person: Person = {
-    name: 'LuisEdoPR',
-    age: 30,
-    twitter: '@LuisEdoPR',
-  };
+  title = 'my application';
+  author = 'LuisEdoPR';
+  count = 0;
+  imgSource = './../favicon.ico';
+  isDisabled = true;
+
 }
